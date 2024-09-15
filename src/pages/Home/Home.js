c93b97a00b3c244c5f96f888279b1b69bc2ce856
Fouza <gf_oukacha@esi.dz>
2024-09-15 21:13:54 +0100

    send req to api

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,27 +1,57 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import DataComp from '../../components/DataComp/DataComp'
 import { CATEGORIES, PRODUCTS } from '../../data/data'
 import Product from '../../components/Product/Product'
+import axios from 'axios'
 
 
 const Home = () => {
     console.log(PRODUCTS)
 
+    const [products, setProducts] = useState([])
+
     useEffect(() => {
-        const mapped_data = new Map()
-        CATEGORIES.forEach(category => {
-            mapped_data.set(category, PRODUCTS.filter(p => p.category_id === category.id))
-        })
-        console.log(mapped_data)
-    })
+        axios.get('http://localhost:8001/api/products')
+            .then(({ data }) => {
+                console.log(data)
+                if (data && data.success) {
+                    setProducts(data.products)
+                }
+            })
+            .catch(error => console.error('Error:', error));
+    }, []);
+
+
     return (
         <div>
             <h2>Home page</h2>
-            {/* <DataComp /> */}
-            {/* {PRODUCTS.map(product => (
+            {products && products.length > 0 ?
+                products.map(item => <h2 key={item.name}>{item.name} - {item.price}</h2>)
+                : <p>...Loading</p>
+            }
+        </div>
+    )
+}
+
+export default Home
+
+
+
+// useEffect(() => {
+//     const mapped_data = new Map()
+//     CATEGORIES.forEach(category => {
+//         mapped_data.set(category, PRODUCTS.filter(p => p.category_id === category.id))
+//     })
+//     console.log(mapped_data)
+// }, [])
+
+
+
+{/* <DataComp /> */ }
+{/* {PRODUCTS.map(product => (
                 <Product key={product.name} product={product} />
             ))} */}
-            {CATEGORIES.map(category => (
+{/* {CATEGORIES.map(category => (
                 <Fragment>
                     <h1>{category.name}</h1>
                     {PRODUCTS.filter(product => product.category_id === category.id)
@@ -29,9 +59,4 @@ const Home = () => {
                             <Product key={`${category.id}_${product.name}`} product={product} />
                         ))}
                 </Fragment>
-            ))}
-        </div>
-    )
-}
-
-export default Home
+            ))} */}
